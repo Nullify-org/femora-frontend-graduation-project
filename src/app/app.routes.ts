dc107662a4e50633755cf50c5106a68bcd8b9201
Mohamed Mostafa <mmostafa9396@gmail.com>
2026-06-14 19:15:08 +0300

    yeslem nour and mohamed efforts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { Register } from './features/auth/pages/register/register';
+import { Interests } from './features/auth/pages/onboarding/interests/interests';
+import { Goal } from './features/auth/pages/onboarding/goal/goal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'onboarding/interests', component: Interests },
+  { path: 'onboarding/goal', component: Goal },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+]
